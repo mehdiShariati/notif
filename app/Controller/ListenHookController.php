9dfb238fc359b2564b73c02fd271e9cd91e53c07
mahdi shariati <mehdishariati12@gmail.com>
2020-06-07 18:12:52 +0430

    edit controller

--- app/Controller/ListenHookController.php
+++ app/Controller/ListenHookController.php
@@ -7,6 +7,7 @@ class ListenHookController extends _Controller
 {
     public function index(Request $request, Response $response, array $args = []):Response
     {
+        Telegram::instantiateTelegram()->send(file_get_contents("php://input"));
         $data=json_decode(file_get_contents("php://input"),true);
         $notif=[
             "type"=>$data["event_type"],
